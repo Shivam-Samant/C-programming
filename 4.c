d3c082b702a7bd4feaf61d85c3f0b54007039680
Shivam-Samant <shivamsamant000@gmail.com>
2021-12-24 19:55:38 +0530

    minor changes

--- 4.c
+++ 4.c
@@ -1,33 +1,27 @@
 //PRINT THE PATTERN
-// 1
-// 11
-// 121
-// 1331
-// 14641
+// 1     
+// 11    
+// 121      
+// 1331  
+// 14641    
 #include<stdio.h>
 int main(){
     int i,j,n;
-    int a[5][5];
     printf("Enter the no. \n");
     scanf(" %d",&n);
-    for(i=0;i<n;i++){
-
-        for(j=0;j<=i;j++){
-        //    if(j==a[i][0]||a[i][j]){a[i][0]=1;a[i][j]=1;printf("%d",a[i][0]);printf("%d",a[i][j]);}
-        
-            /* else*/ if(j!=a[i][0]&&a[i][j]) {
-                        if(i>0 && j<n){a[i][j]=i; 
-                        printf("%d",i);}
-                        }
-                   else if(j==a[(n+1/2)][n+1/2]){
-                   a[n+1/2][n+1/2]=(a[i][j-1])+(a[i-1][j]);
-                   printf("%d",a[(n+1/2)][n+1/2]);}
-                   else {printf("%d",1);} 
-                
-             }
-    printf("\n");
+    int a[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            if(j==0 || j==i) {
+                a[i][j]=1;
+                printf("%d",a[i][j]);
+                }
+            else {
+                a[i][j]=a[i-1][j-1] + a[i-1][j];
+                printf("%d",a[i][j]);
+                }
         }
-        a[0][0]=1;
-        printf("%d",a[0][0]);
+    printf("\n");
+    }
     return 0;
 }
